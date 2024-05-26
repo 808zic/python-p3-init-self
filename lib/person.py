3e7dfb0f9ff7a0cea1334106fa62b3afb5d97464
Georgemugo <georgepmash@gmail.com>
2024-05-26 18:58:09 +0300

    Adds validation

--- lib/person.py
+++ lib/person.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    def __init__(self, name, Planet="Earth"):
+    def __init__(self, name, Planet="Earth", species="Human"):
         self.name = name
         self.Planet = Planet
+        self.species = species
 
-zic = Person("808","Cybertron")
-zic.name
-zic.Planet
+zic = Person("zic_808","Cybertron", "Saiyan")
+print (zic.name)
+print (zic.Planet)
+print(zic.species)
 
